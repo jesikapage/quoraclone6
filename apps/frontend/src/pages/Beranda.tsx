abe549b8f439084ec73e3714aafae23f2478a7f7
priliapuspita04-stack <priliapuspita04.com>
2026-05-26 14:36:38 +0700

    rename ke kapital

--- apps/frontend/src/pages/Beranda.tsx
+++ apps/frontend/src/pages/Beranda.tsx
@@ -6,7 +6,7 @@ import {
   MoreHorizontal, X, HelpCircle, PenLine, Send,
 } from "lucide-react";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/footbar";
+import Sidebar from "../components/Sidebar";
 
 type Post = {
   id: string;
