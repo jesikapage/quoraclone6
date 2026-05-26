610b33db9a3f5f7e5867703974a6d94af94c6724
mas-rito <ritoramadhan8@gmail.com>
2026-05-26 15:11:41 +0700

    feat(db): clear migration db to rds

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "prisma/config";
-import * as dotenv from "dotenv";
 
-dotenv.config();
+const dbUrl = process.env.DATABASE_URL || "file:./dev.db";
+const schema = dbUrl.startsWith("postgre")
+  ? "prisma/schema-postgres.prisma"
+  : "prisma/schema.prisma";
+
+// --- LOG UNTUK DEBUGGING ---
+console.log("==========================================");
+console.log("DATABASE_URL :", process.env.DATABASE_URL);
+console.log("Skema        :", schema);
+console.log("==========================================");
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: schema,
   migrations: {
     path: "prisma/migrations",
+    seed: "bun run prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: dbUrl,
   },
 });
