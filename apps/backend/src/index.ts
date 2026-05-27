8ef95e7c822d915fd824a4c16a8c1db53717ab3a
jesikapage <jesikatan90@gmail.com>
2026-05-27 13:46:55 +0700

    Logika Google

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import * as dotenv from "dotenv";
 import { commentRoutes } from "./routes/comment.routes";
 
@@ -7,10 +7,45 @@ dotenv.config();
 const app = new Elysia()
   .get("/", () => ({ message: "API is running alias berjalan 🚀 via Lambda Function URL" }))
 
+  .post("/auth/google", async ({ body, set }) => {
+    const { token } = body;
+
+    try {
+      const googleRes = await globalThis.fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`);
+      
+      if (!googleRes.ok) {
+        set.status = 401;
+        return { success: false, message: "Token Google tidak valid atau kedaluwarsa" };
+      }
+
+      const googleUser = (await googleRes.json()) as {
+        name: string;
+        email: string;
+        picture: string;
+      };
+
+      return {
+        success: true,
+        message: "Login Google Berhasil!",
+        user: {
+          name: googleUser.name,
+          email: googleUser.email,
+          picture: googleUser.picture
+        }
+      };
+
+    } catch (error) {
+      set.status = 500;
+      return { success: false, error: "INTERNAL_SERVER_ERROR", message: String(error) };
+    }
+  }, {
+    body: t.Object({
+      token: t.String()
+    })
+  })
+
   .get("/users", ({ query, set }) => {
     const secretKey = query.key;
-    
-
     const VALID_KEY = "asdos-ppwl-rahasia-2026"; 
 
     if (!secretKey || secretKey !== VALID_KEY) {
@@ -49,9 +84,12 @@ const app = new Elysia()
     return { success: false, error: "INTERNAL_SERVER_ERROR" };
   });
 
-
 export const fetch = async (request: Request) => {
   return await app.handle(request);
+};
+
+export default {
+  fetch
 };
 
 export type App = typeof app;
